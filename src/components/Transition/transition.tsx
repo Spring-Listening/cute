7eda0d3502b5c5711c40b0d7dfbe0ab7acc3be0d
zoucw <326359613@qq.com>
2022-01-19 13:43:29 +0800

    修改ts报错

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -3,19 +3,21 @@
  * @version: 
  * @Author: zoucw (326359613@qq.com)
  * @Date: 2021-02-18 00:31:14
- * @LastEditors: 
- * @LastEditTime: 2021-02-18 23:43:04
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-19 11:08:47
  */
 import React from 'react'
 import { CSSTransition } from 'react-transition-group'
-import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
+import { TransitionProps } from "react-transition-group/Transition";
 
-type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right'
-
-interface TransitionProps extends CSSTransitionProps {
-  animation?: AnimationName,
-  wrapper? : boolean,
-}
+// type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right'
+// interface CSSTransitionProps {
+//   classNames?: string | CSSTransitionClassNames | undefined;
+// };
+// interface TransitionProps extends CSSTransitionProps {
+//   animation?: AnimationName,
+//   wrapper? : boolean,
+// }
 
 const Transition: React.FC<TransitionProps> = (props) => {
   const {
